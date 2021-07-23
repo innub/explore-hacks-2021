36b18dcca0e54dedb2e086d8c688f3af5533c439
Kevin G <ge.kevin.a@gmail.com>
2021-07-23 13:48:05 -0500

    Added colors to constants

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const purple200 = Color(0x8B35C0);
+const purple150 = Color(0xA742E4);
+const purple100 = Color(0xB67ADB);
+const purple50 = Color(0xFDF5FF);
+
+const grey150 = Color(0x5F5F5F);
+const grey100 = Color(0x767676);
